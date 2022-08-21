50d7a5036a914d14a283a9ab9c6b8040999698de
حسن المسبح <hsnalmsbh@Hassans-MacBook-Air.local>
2022-08-21 12:29:30 +0300

    l

--- index.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-const files = fs.readdirSync("./");
-const timesDone = 1;
-fs.writeFile('.gitignore', files.splice(1000).join('\n'), ()=>console.log('done'))
-console.log(files.length)
